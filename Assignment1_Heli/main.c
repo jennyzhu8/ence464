2814a571bcbdd5f7f3f28efd16111dd4943516be
Bradley Thomas <bth74@uclive.ac.nz>
2023-08-03 16:09:59 +1200

    ADC and PWM going

--- Assignment1_Heli/main.c
+++ Assignment1_Heli/main.c
@@ -16,6 +16,26 @@
 
 #define BUF_SIZE 10
 
+// PWM configuration
+#define PWM_START_RATE_HZ  80
+#define PWM_RATE_STEP_HZ   50
+#define PWM_RATE_MIN_HZ    50
+#define PWM_RATE_MAX_HZ    400
+#define PWM_FIXED_DUTY     85
+#define PWM_DIVIDER_CODE   SYSCTL_PWMDIV_4
+#define PWM_DIVIDER        4
+//  PWM Hardware Details M0PWM7 (gen 3)
+//  ---Main Rotor PWM: PC5, J4-05
+#define PWM_MAIN_BASE        PWM0_BASE
+#define PWM_MAIN_GEN         PWM_GEN_3
+#define PWM_MAIN_OUTNUM      PWM_OUT_7
+#define PWM_MAIN_OUTBIT      PWM_OUT_7_BIT
+#define PWM_MAIN_PERIPH_PWM  SYSCTL_PERIPH_PWM0
+#define PWM_MAIN_PERIPH_GPIO SYSCTL_PERIPH_GPIOC
+#define PWM_MAIN_GPIO_BASE   GPIO_PORTC_BASE
+#define PWM_MAIN_GPIO_CONFIG GPIO_PC5_M0PWM7
+#define PWM_MAIN_GPIO_PIN    GPIO_PIN_5
+
 /**
  * main.c
  */
@@ -23,6 +43,10 @@
 static void NullTaskFunc(void *);
 static void Blink_LED_task(void *);
 static void ADC_task(void *);
+void initialisePWM (void);
+void setPWM (uint32_t u32Freq, uint32_t u32Duty);
+void initialiseLED (void);
+void initialiseADC (void);
 
 void
 ConfigureUART(void)
@@ -55,13 +79,61 @@ ConfigureUART(void)
     UARTStdioConfig(0, 115200, 16000000);
 }
 
-int main(void)
+void
+initialisePWM (void)
 {
+    SysCtlPeripheralEnable(PWM_MAIN_PERIPH_PWM);
+    SysCtlPeripheralEnable(PWM_MAIN_PERIPH_GPIO);
 
-    // Set the clock rate to 80 MHz
-    SysCtlClockSet (SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+    GPIOPinConfigure(PWM_MAIN_GPIO_CONFIG);
+    GPIOPinTypePWM(PWM_MAIN_GPIO_BASE, PWM_MAIN_GPIO_PIN);
 
-    ConfigureUART();
+    PWMGenConfigure(PWM_MAIN_BASE, PWM_MAIN_GEN,
+                    PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC);
+    // Set the initial PWM parameters
+    setPWM (PWM_START_RATE_HZ, PWM_FIXED_DUTY);
+
+    PWMGenEnable(PWM_MAIN_BASE, PWM_MAIN_GEN);
+
+    // Disable the output.  Repeat this call with 'true' to turn O/P on.
+    PWMOutputState(PWM_MAIN_BASE, PWM_MAIN_OUTBIT, false);
+
+}
+
+/********************************************************
+ * Function to set the freq, duty cycle of M0PWM7
+ ********************************************************/
+void
+setPWM (uint32_t ui32Freq, uint32_t ui32Duty)
+{
+    // Calculate the PWM period corresponding to the freq.
+    uint32_t ui32Period = SysCtlClockGet() / PWM_DIVIDER / ui32Freq;
+
+    PWMGenPeriodSet(PWM_MAIN_BASE, PWM_MAIN_GEN, ui32Period);
+    PWMPulseWidthSet(PWM_MAIN_BASE, PWM_MAIN_OUTNUM, ui32Period * ui32Duty / 100);
+}
+
+void
+initialiseLED (void)
+{
+    // For LED blinky task - initialize GPIO port F and then pin #1 (red) for output
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)) ; // busy-wait until GPIOF's bus clock is ready
+
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1); // PF_1 as output
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2); // PF_2 as output
+    // doesn't need too much drive strength as the RGB LEDs on the TM4C123 launchpad are switched via N-type transistors
+
+    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_STRENGTH_4MA, GPIO_PIN_TYPE_STD);
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0); // off by default
+    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_STRENGTH_4MA, GPIO_PIN_TYPE_STD);
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0); // off by default
+
+}
+
+void
+initialiseADC (void)
+{
     //
     // Enable the ADC0 module.
     //
@@ -80,20 +152,21 @@ int main(void)
     ADCSequenceStepConfigure(ADC0_BASE, 0, 0,ADC_CTL_IE | ADC_CTL_END | ADC_CTL_CH9);
     ADCSequenceEnable(ADC0_BASE, 0);
 
-    // For LED blinky task - initialize GPIO port F and then pin #1 (red) for output
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)) ; // busy-wait until GPIOF's bus clock is ready
-
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1); // PF_1 as output
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2); // PF_2 as output
-    // doesn't need too much drive strength as the RGB LEDs on the TM4C123 launchpad are switched via N-type transistors
-
-    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_STRENGTH_4MA, GPIO_PIN_TYPE_STD);
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0); // off by default
-    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_STRENGTH_4MA, GPIO_PIN_TYPE_STD);
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0); // off by default
+}
 
 
+int main(void)
+{
+
+    // Set the clock rate to 80 MHz
+    SysCtlClockSet (SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+
+    ConfigureUART();
+    initialisePWM();
+    initialiseLED();
+    initialiseADC();
+
+    PWMOutputState(PWM_MAIN_BASE, PWM_MAIN_OUTBIT, true);
 
 
     if (pdTRUE != xTaskCreate(Blink_LED_task, "Blinker Blue", 32, (void *)2, 4, NULL))
@@ -183,13 +256,18 @@ static void ADC_task(void *pvParameters)
             alt = alt + alt_buffer[i];
         }
         alt = alt / BUF_SIZE;
-        // Min Altitude = 2860, Max Altitude = 1200
-        //height = (1/alt)*1200; // Not sure about this, trying to get a percentage of height
-        
+        // Values depend on the rig you are using
+        // Min Altitude = 2230, Max Altitude = 1000
+        // For emulator to 100%
+        // Min Alt = 3030, Max alt = 1900
+        // Emulator to max which is 133%
+        // Min alt =3030, max alt = 1330
+
+
         //getting height between 1660 and 0
-        height = alt-1200;
+        height = alt-1330;
         //getting height as a percentage
-        height = 100 * (1 - (height/1660));
+        height =  (133 - ((height*133)/1700));
 
         UARTprintf("Altitude = %d ", height);
         vTaskDelay(200);
