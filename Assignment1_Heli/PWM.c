679757f8d0ae1b5043605ea102295f5ee34d4b5c
Bradley Thomas <bth74@uclive.ac.nz>
2023-08-08 17:04:04 +1200

    Refactoring PWM

---
+++ Assignment1_Heli/PWM.c
@@ -0,0 +1,65 @@
+/*
+ * PWM.c
+ *
+ *  Created on: 8/08/2023
+ *      Author: bth74
+ */
+
+#include <FreeRTOS.h>
+#include <task.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_gpio.h"
+#include "driverlib/adc.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/pwm.h"
+#include "driverlib/sysctl.h"
+#include "utils/uartstdio.h"
+#include "driverlib/rom.h"
+#include "driverlib/uart.h"
+#include "drivers/buttons.h"
+#include "queue.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "priorities.h"
+#include "semphr.h"
+#include "PWM.h"
+
+
+void
+initialisePWM (void)
+{
+    SysCtlPeripheralEnable(PWM_MAIN_PERIPH_PWM);
+    SysCtlPeripheralEnable(PWM_MAIN_PERIPH_GPIO);
+
+    GPIOPinConfigure(PWM_MAIN_GPIO_CONFIG);
+    GPIOPinTypePWM(PWM_MAIN_GPIO_BASE, PWM_MAIN_GPIO_PIN);
+
+    PWMGenConfigure(PWM_MAIN_BASE, PWM_MAIN_GEN,
+                    PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC);
+    // Set the initial PWM parameters
+    setPWM (PWM_START_RATE_HZ, PWM_FIXED_DUTY);
+
+    PWMGenEnable(PWM_MAIN_BASE, PWM_MAIN_GEN);
+
+    // Disable the output.  Repeat this call with 'true' to turn O/P on.
+    PWMOutputState(PWM_MAIN_BASE, PWM_MAIN_OUTBIT, false);
+
+}
+
+/********************************************************
+ * Function to set the freq, duty cycle of M0PWM7
+ ********************************************************/
+void
+setPWM (uint32_t ui32Freq, uint32_t ui32Duty)
+{
+    // Calculate the PWM period corresponding to the freq.
+    uint32_t ui32Period = SysCtlClockGet() / PWM_DIVIDER / ui32Freq;
+
+    PWMGenPeriodSet(PWM_MAIN_BASE, PWM_MAIN_GEN, ui32Period);
+    PWMPulseWidthSet(PWM_MAIN_BASE, PWM_MAIN_OUTNUM, ui32Period * ui32Duty / 100);
+}
--- Assignment1_Heli/main.c
+++ Assignment1_Heli/main.c
@@ -20,6 +20,7 @@
 #include "task.h"
 #include "priorities.h"
 #include "semphr.h"
+#include "PWM.h"
 
 
 #define BUF_SIZE 5
@@ -40,23 +41,8 @@ xSemaphoreHandle g_pUARTSemaphore;
 
 // PWM configuration
 #define PWM_START_RATE_HZ  250
-#define PWM_RATE_STEP_HZ   50
-#define PWM_RATE_MIN_HZ    50
-#define PWM_RATE_MAX_HZ    400
-#define PWM_FIXED_DUTY     75
-#define PWM_DIVIDER_CODE   SYSCTL_PWMDIV_4
-#define PWM_DIVIDER        4
-//  PWM Hardware Details M0PWM7 (gen 3)
-//  ---Main Rotor PWM: PC5, J4-05
-#define PWM_MAIN_BASE        PWM0_BASE
-#define PWM_MAIN_GEN         PWM_GEN_3
-#define PWM_MAIN_OUTNUM      PWM_OUT_7
-#define PWM_MAIN_OUTBIT      PWM_OUT_7_BIT
-#define PWM_MAIN_PERIPH_PWM  SYSCTL_PERIPH_PWM0
-#define PWM_MAIN_PERIPH_GPIO SYSCTL_PERIPH_GPIOC
-#define PWM_MAIN_GPIO_BASE   GPIO_PORTC_BASE
-#define PWM_MAIN_GPIO_CONFIG GPIO_PC5_M0PWM7
-#define PWM_MAIN_GPIO_PIN    GPIO_PIN_5
+
+
 
 // Buttons (from buttons4.h which we should use instead of buttons.h)
 // UP button
@@ -118,39 +104,9 @@ ConfigureUART(void)
     UARTStdioConfig(0, 115200, 16000000);
 }
 
-void
-initialisePWM (void)
-{
-    SysCtlPeripheralEnable(PWM_MAIN_PERIPH_PWM);
-    SysCtlPeripheralEnable(PWM_MAIN_PERIPH_GPIO);
 
-    GPIOPinConfigure(PWM_MAIN_GPIO_CONFIG);
-    GPIOPinTypePWM(PWM_MAIN_GPIO_BASE, PWM_MAIN_GPIO_PIN);
 
-    PWMGenConfigure(PWM_MAIN_BASE, PWM_MAIN_GEN,
-                    PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC);
-    // Set the initial PWM parameters
-    setPWM (PWM_START_RATE_HZ, PWM_FIXED_DUTY);
 
-    PWMGenEnable(PWM_MAIN_BASE, PWM_MAIN_GEN);
-
-    // Disable the output.  Repeat this call with 'true' to turn O/P on.
-    PWMOutputState(PWM_MAIN_BASE, PWM_MAIN_OUTBIT, false);
-
-}
-
-/********************************************************
- * Function to set the freq, duty cycle of M0PWM7
- ********************************************************/
-void
-setPWM (uint32_t ui32Freq, uint32_t ui32Duty)
-{
-    // Calculate the PWM period corresponding to the freq.
-    uint32_t ui32Period = SysCtlClockGet() / PWM_DIVIDER / ui32Freq;
-
-    PWMGenPeriodSet(PWM_MAIN_BASE, PWM_MAIN_GEN, ui32Period);
-    PWMPulseWidthSet(PWM_MAIN_BASE, PWM_MAIN_OUTNUM, ui32Period * ui32Duty / 100);
-}
 
 void
 initialiseLED (void)
@@ -320,7 +276,6 @@ static void ADC_task(void *pvParameters)
     uint8_t index = 0;
     int32_t alt = 0;
     uint32_t height;
-    uint32_t pwm;
     uint32_t count;
     uint8_t message;
     uint32_t max_voltage;
@@ -533,7 +488,9 @@ static void Get_PWM_Task(void *pvParameters)
     {
         if(xQueueReceive(g_pLEDQueue, &i8Message, pdMS_TO_TICKS(125)) == pdPASS) // ticks to wait must be > 0 so the task doesn't get stuck here
         {
+            xSemaphoreTake(g_pUARTSemaphore, portMAX_DELAY);
             UARTprintf("Altitude = %d\n", i8Message);
+            xSemaphoreGive(g_pUARTSemaphore);
             vTaskDelayUntil(&ui16LastTime, ui32SwitchDelay / portTICK_RATE_MS);
         }
         height = i8Message;
@@ -541,8 +498,6 @@ static void Get_PWM_Task(void *pvParameters)
         if (pwm > 80)
         {
             pwm = 80;
-        } else if (pwm < 0) {
-            pwm = 0;
         }
 
         setPWM (PWM_START_RATE_HZ, pwm);
